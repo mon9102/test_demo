574c1789218b05ef1a51516fdf06c544144611a1
zouren8272687@163.com <zour851374962>
2019-04-03 09:55:02 +0800

    update

--- https/src/main/java/com/sinosoft/testhttpsApplication.java
+++ https/src/main/java/com/sinosoft/testhttpsApplication.java
@@ -19,6 +19,7 @@ import java.io.InputStreamReader;
 @SpringBootApplication
 public class testhttpsApplication {
     public static void main(String[] args) {
+        System.setProperty("javax.net.debug","ssl");
         SpringApplication.run(testhttpsApplication.class,args);
 
     }
