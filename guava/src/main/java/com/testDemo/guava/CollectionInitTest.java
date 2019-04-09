dbfdcf7be657e3f04b98f48d1204620ba28b1381
zouren8272687@163.com <zour851374962>
2019-04-09 15:42:12 +0800

    update

--- guava/src/main/java/com/testDemo/guava/CollectionInitTest.java
+++ guava/src/main/java/com/testDemo/guava/CollectionInitTest.java
@@ -1,8 +1,6 @@
 package com.testDemo.guava;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
+import com.google.common.collect.*;
 import jdk.nashorn.internal.objects.annotations.Getter;
 import jdk.nashorn.internal.objects.annotations.Setter;
 
@@ -17,20 +15,42 @@ import java.util.Set;
  */
 public class CollectionInitTest {
     public static void main(String[] args) {
-        Set<String> set = Sets.newHashSet("one","two","three");
 
-        List<String> list = Lists.newArrayList("one","two","three");
+    }
 
-        Map<String, String> map = ImmutableMap.of("ON","TRUE","OFF","FALSE");
+    /**
+     * 集合创建
+     */
+    public void creat() {
+        Map<String, Map<String, String>> map = Maps.newHashMap();
+
+        List<List<Map<String, String>>> list = Lists.newArrayList();
+
+//1,简化集合的创建
+        List<Person> personList = Lists.newLinkedList();
+        Set<Person> personSet = Sets.newHashSet();
+        Map<String, Person> personMap = Maps.newHashMap();
+        Integer[] intArrays = ObjectArrays.newArray(Integer.class, 10);
+    }
+
+    /**
+     * 集合初始化
+     */
+    public void init() {
+        Set<String> set = Sets.newHashSet("one", "two", "three");
+
+        List<String> list = Lists.newArrayList("one", "two", "three");
+
+        Map<String, String> map = ImmutableMap.of("ON", "TRUE", "OFF", "FALSE");
 
         //2,简化集合的初始化
-        List<Person> personList2= Lists.newArrayList(new Person(1, 1, "a", "46546", 1, 20),
+        List<Person> personList2 = Lists.newArrayList(new Person(1, 1, "a", "46546", 1, 20),
 
                 new Person(2, 1, "a", "46546", 1, 20));
-        Set<Person> personSet2= Sets.newHashSet(new Person(1,1,"a","46546",1,20),
+        Set<Person> personSet2 = Sets.newHashSet(new Person(1, 1, "a", "46546", 1, 20),
 
-                new Person(2,1,"a","46546",1,20));
-        Map<String,Person> personMap2= ImmutableMap.of("hello",new Person(1,1,"a","46546",1,20),"fuck",new Person(2,1,"a","46546",1,20));
+                new Person(2, 1, "a", "46546", 1, 20));
+        Map<String, Person> personMap2 = ImmutableMap.of("hello", new Person(1, 1, "a", "46546", 1, 20), "fuck", new Person(2, 1, "a", "46546", 1, 20));
     }
 }
 
@@ -41,13 +61,14 @@ class Person {
     private String name1;
     private int a;
     private int b;
-    public Person(int group,int id,String name,String name1,int a,int b){
-        this.group=group;
-        this.id=id;
-        this.name=name;
-        this.name1=name1;
-        this.a=a;
-        this.b=b;
+
+    public Person(int group, int id, String name, String name1, int a, int b) {
+        this.group = group;
+        this.id = id;
+        this.name = name;
+        this.name1 = name1;
+        this.a = a;
+        this.b = b;
 
     }
 
