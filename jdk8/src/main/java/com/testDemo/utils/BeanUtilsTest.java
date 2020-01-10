ef63fde0a77ff10e3916d7cfd8ec2039456ad9ea
zouren8272687@163.com <zour851374962>
2020-01-10 21:13:42 +0800

    update

--- jdk8/src/main/java/com/testDemo/utils/BeanUtilsTest.java
+++ jdk8/src/main/java/com/testDemo/utils/BeanUtilsTest.java
@@ -1,9 +1,26 @@
 package com.testDemo.utils;
 
+import com.testDemo.jdk8.enums.MySymbol;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
 /**
  * @Auther: zouren
  * @Date: 2020/1/10 17:33
  * @Description:
  */
 public class BeanUtilsTest {
+    @Test
+    public void test1()throws Exception{
+        Mt4Rule mt4Rule = new Mt4Rule();
+        mt4Rule.setjBaiyin(1D);
+        mt4Rule.setzBaiyin(2D);
+        String str = MySymbol.BAIYIN.name().toLowerCase();
+
+        System.out.println(str);
+        System.out.println(  StringUtils.join("z",str.substring(0,1).toUpperCase(),str.substring(1)));
+        System.out.println(PropertyUtils.getSimpleProperty(mt4Rule,"zbaiyin"));
+    }
 }
---
+++ jdk8/src/main/java/com/testDemo/utils/Mt4Rule.java
@@ -0,0 +1,108 @@
+package com.testDemo.utils;
+
+public class Mt4Rule  {
+
+
+	public Double zHuangjin;
+
+	public Double zWaihui;
+
+	public Double zYuanyou;
+
+	public Double zBaiyin;
+
+	public Double jHuangjin;
+
+	public Double jWaihui;
+
+	public Double jYuanyou;
+
+	public Double jBaiyin;
+
+	public String createUser;
+
+	public String updateUser;
+
+	public Double getzHuangjin() {
+		return zHuangjin;
+	}
+
+	public void setzHuangjin(Double zHuangjin) {
+		this.zHuangjin = zHuangjin;
+	}
+
+	public Double getzWaihui() {
+		return zWaihui;
+	}
+
+	public void setzWaihui(Double zWaihui) {
+		this.zWaihui = zWaihui;
+	}
+
+	public Double getzYuanyou() {
+		return zYuanyou;
+	}
+
+	public void setzYuanyou(Double zYuanyou) {
+		this.zYuanyou = zYuanyou;
+	}
+
+	public Double getzBaiyin() {
+		return zBaiyin;
+	}
+
+	public void setzBaiyin(Double zBaiyin) {
+		this.zBaiyin = zBaiyin;
+	}
+
+	public Double getjHuangjin() {
+		return jHuangjin;
+	}
+
+	public void setjHuangjin(Double jHuangjin) {
+		this.jHuangjin = jHuangjin;
+	}
+
+	public Double getjWaihui() {
+		return jWaihui;
+	}
+
+	public void setjWaihui(Double jWaihui) {
+		this.jWaihui = jWaihui;
+	}
+
+	public Double getjYuanyou() {
+		return jYuanyou;
+	}
+
+	public void setjYuanyou(Double jYuanyou) {
+		this.jYuanyou = jYuanyou;
+	}
+
+	public Double getjBaiyin() {
+		return jBaiyin;
+	}
+
+	public void setjBaiyin(Double jBaiyin) {
+		this.jBaiyin = jBaiyin;
+	}
+
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public String getUpdateUser() {
+		return updateUser;
+	}
+
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+
+
+
+}
