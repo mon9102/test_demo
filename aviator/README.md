9c79ac5224df1fde7763dc451a007f373802436e
itw_zouren <zour851374962>
2020-05-09 15:12:39 +0800

    update

---
+++ aviator/README.md
@@ -0,0 +1,5 @@
+# AviatorScript 编程指南（5.0）
+
+https://www.yuque.com/boyan-avfmj/aviatorscript/cpow90
+
+脚本执行引擎
---
+++ aviator/src/test/java/com/test/RunTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author : zouren
+ * @date : 2020/5/9 10:38
+ */
+public class RunTest {
+
+    public void aa(){
+
+    }
+}
