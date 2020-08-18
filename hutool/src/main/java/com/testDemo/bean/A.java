a8313ab736e8e81dff4fce14df3db0bfa4382b03
bj-zour <bj-zour>
2020-08-18 15:20:34 +0800

    update

---
+++ hutool/src/main/java/com/testDemo/bean/A.java
@@ -0,0 +1,19 @@
+package com.testDemo.bean;
+
+/**
+ * @author: zouren
+ * @date: 2020/8/18 10:34
+ * @description:
+ */
+
+public class A {
+    private String codename;
+
+    public String getCodename() {
+        return codename;
+    }
+
+    public void setCodename(String codename) {
+        this.codename = codename;
+    }
+}
---
+++ hutool/src/main/java/com/testDemo/bean/A2.java
@@ -0,0 +1,18 @@
+package com.testDemo.bean;
+
+/**
+ * @author: zouren
+ * @date: 2020/8/18 10:36
+ * @description:
+ */
+public class A2 {
+    private String codeName;
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+}
